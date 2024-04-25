f2c74d6803263da59949dca6114c1fe16e14fac2
NAGARRO\akshaysrivastava <akshaysrivastava46@gmail.com>
2024-04-25 10:54:02 +0530

    initial files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
+});
+
+app.get("/", (request, response) => {
+  const status = {
+    Status: "Running",
+  };
+  response.send(status);
+});
